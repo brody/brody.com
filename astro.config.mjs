d20565aa6f5083e7ec75324324286259c8e3546a
Brody MacLean <hello@brody.com>
2025-10-14 21:05:21 +1100

    Change output mode to server for Netlify compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import netlify from '@astrojs/netlify'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://brody.com',
-  output: 'hybrid',
+  output: 'server',
   adapter: netlify(),
   integrations: [mdx(), preact(), icon(), tunnel()],
 
